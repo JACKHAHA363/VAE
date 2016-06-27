f3aaa181ef7db6153a6801629d131c7103180ce0
Yuchen Lu <lu72@illinois.edu>
2016-06-27 16:24:41 -0500

    unclear about the network structure

--- vae_yz_x.py
+++ vae_yz_x.py
@@ -1,3 +1,6 @@
+# A vae_yz_x example for ssl for mnist. The network
+# architecture is the same as original paper
+
 import numpy as np
 import tensorflow as tf
 
@@ -6,6 +9,11 @@ get_ipython().magic('matplotlib inline')
 
 np.random.seed(0)
 tf.set_random_seed(0)
+
+INPUT_SIZE = 784
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("data/", one_hot=True)
 
 def xavier_init(fan_in, fan_out, constant=1): 
     """ Xavier initialization of network weights"""
@@ -16,7 +24,43 @@ def xavier_init(fan_in, fan_out, constant=1):
                              minval=low, maxval=high, 
                              dtype=tf.float32)
 
-
 class Vae_yz_x(object):
     
-    def __init__(self, n_x, n_y, n_z, network_config, )
+    def __init__(self, config):
+        self.config = config
+        self.initialize_weights()
+        self.build_model()
+
+    def build_model(self):
+        """ hold input/output data """
+        self.x = tf.placeholder(tf.float32, shape=(self.config["batch_size"],784))
+        self.y = tf.placeholder(tf.float32, shape=(self.config["batch_size"],10))
+    
+    def initialize_weights(self):
+        """ parameters for the recognition_network """
+        self.phi = dict(
+                weight_x = tf.Variable(xavier_init(784, self.config["n_hidden_recog"]), name="recog_w_x"),
+                weight_y = tf.Variable(xavier_init(10, self.config["n_hidden_recog"]), name="recog_w_y"),
+                recog_b = tf.Variable(tf.zeros([self.config["n_hidden_recog"]]), name="recog_b")
+                )
+
+        """ parameters for the generative_network """
+        self.theta = dict(
+                
+                )
+    
+    def z_recognition_network(self):
+        
+        z_mean = tf.softplus(
+                tf.add(
+                    self.phi["recog_b"], 
+                    tf.add(
+                        tf.matmul(self.x, self.phi["weight_x"]), 
+                        tf.matmul(self.y, self.phi["weight_y"])
+                        )
+                    )
+                )
+        
+        
+
+        
