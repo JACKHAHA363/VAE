65afd3a7f53ae489ef97ece0404c689cab0ee31e
Yuchen Lu <he@he.com>
2016-08-28 15:45:39 -0700

    start debugging why NAN

--- run_vae.py
+++ run_vae.py
@@ -7,13 +7,12 @@ import tensorflow as tf
 
 import os
 import numpy as np
-np.random.seed(0)
-tf.set_random_seed(0)
+#np.random.seed(0)
+#tf.set_random_seed(0)
 
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 n_samples = mnist.train.num_examples
-
 
 network_architecture = dict(
         n_hidden_recog_1=500,
@@ -23,33 +22,76 @@ network_architecture = dict(
         n_input=784, 
         n_z=20
         )
+
 learning_rate=0.001
 batch_size=100
-training_epochs=70
+training_epochs=1
 display_step=5
 
-vae = VariationalAutoencoder(
-        network_architecture, 
-        learning_rate=learning_rate,
-        batch_size=batch_size)
+def NoiseContrastiveLoss(data_model, noise_model):
+    pos_term = tf.neg(tf.log(
+        1.0 + tf.exp(noise_model.ll_pos - data_model.ll_pos)
+        ))
 
-# training vinilla VAE
+    neg_term = tf.neg(tf.log(
+        1.0 + tf.exp(data_model.ll_neg - noise_model.ll_neg)
+        ))
+    return tf.neg(tf.reduce_mean(pos_term) + tf.reduce_mean(neg_term))
 
-result = []
-for epoch in range(training_epochs):
-    avg_lb = 0.
-    total_batch = int(n_samples / batch_size)
-    # Loop over all batches
-    for i in range(total_batch):
-        batch_xs, _ = mnist.train.next_batch(batch_size)
 
-        lb = vae.partial_fit(batch_xs)
-        # Compute average loss
-        avg_lb += lb / n_samples * batch_size
+S = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
 
-    # Display logs per epoch step
-    if epoch % display_step == 0:
-        print("Epoch:", '%04d' % (epoch+1), "lower bound=", "{:.9f}".format(avg_lb))
+with tf.device("/gpu:0"):
+    neg_example = tf.placeholder(tf.float32, [batch_size, network_architecture["n_input"]])
+    pos_example = tf.placeholder(tf.float32, [batch_size, network_architecture["n_input"]])
+    vae_data = VariationalAutoencoder(
+            network_architecture=network_architecture, 
+            transfer_fct=tf.nn.softplus,
+            batch_size=batch_size,
+            session=S, 
+            trainable=True,
+            pos_example=pos_example, 
+            neg_example=neg_example
+            )
+    
+    vae_noise = VariationalAutoencoder(
+            network_architecture=network_architecture, 
+            transfer_fct=tf.nn.softplus,
+            batch_size=batch_size,
+            session=S, 
+            trainable=False,
+            pos_example=pos_example, 
+            neg_example=neg_example
+            )
+        
+    loss = NoiseContrastiveLoss(vae_data, vae_noise)
+    optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+    
+    init = tf.initialize_all_variables()
+    S.run(init)
+ 
+    for epoch in range(training_epochs):
+        avg_loss = 0
+        total_batch = int(n_samples / batch_size)
 
-    result.append(avg_lb)
+        for i in range(1):
+            batch_data, _ = mnist.train.next_batch(batch_size)
+            batch_noise = vae_noise.reconstruct(batch_data)
+            
+            import collections
+            print(collections.Counter(batch_noise[0]))
+
+#            _, loss_eval = S.run(
+#                    [optimizer, loss], 
+#                    {pos_example : batch_data, neg_example : batch_noise}
+#                    )
+#            
+#            avg_loss += loss_eval / n_samples * batch_size
+#            
+#            print(loss_eval)
+
+#         if epoch % display_step == 0:
+#            print("Epoch:", '%04d' % (epoch+1), "nc loss =", "{:.9f}".format(avg_loss))
+
+
 
--- vae.py
+++ vae.py
@@ -16,118 +16,132 @@ def xavier_init(fan_in, fan_out, constant=1):
 
 class VariationalAutoencoder(object):
 
-    def __init__(self, network_architecture, transfer_fct=tf.nn.softplus, 
-                 learning_rate=0.001, batch_size=100):
+    def __init__(self, network_architecture, transfer_fct, batch_size, session, trainable, pos_example, neg_example):
         self.network_architecture = network_architecture
         self.transfer_fct = transfer_fct
-        self.learning_rate = learning_rate
         self.batch_size = batch_size
-        
-        # tf Graph input
-        self.x = tf.placeholder(tf.float32, [batch_size, network_architecture["n_input"]])
-        
+        self.trainable=trainable
+        self.sess = session
+        self.pos_example = pos_example
+        self.neg_example = neg_example
+
         # Create autoencoder network
         self._create_network()
-        # Define loss function based variational upper-bound and 
-        # corresponding optimizer
-        self._create_loss_optimizer()
-        
-        # Initializing the tensor flow variables
-        init = tf.initialize_all_variables()
 
-        # Launch the session
-        self.sess = tf.InteractiveSession()
-        self.sess.run(init)
-    
+        self._create_log_likelihood()
+   
     def _create_network(self):
 
         network_weights = self._initialize_weights(**self.network_architecture)
-
-        self.z_mean, self.z_log_sigma_sq = self._recognition_network(network_weights["weights_recog"], network_weights["biases_recog"])
-
         n_z = self.network_architecture["n_z"]
         eps = tf.random_normal((self.batch_size, n_z), 0, 1, 
                                dtype=tf.float32)
 
-        self.z = tf.add(self.z_mean, 
-                        tf.mul(tf.sqrt(tf.exp(self.z_log_sigma_sq)), eps))
+        # graph for pos_example
+        self.z_mean_pos, self.z_log_sigma_sq_pos = self._recognition_network(
+                network_weights["weights_recog"], 
+                network_weights["biases_recog"], 
+                self.pos_example)
+ 
+        self.z_pos = tf.add(
+                self.z_mean_pos, 
+                tf.mul(tf.sqrt(tf.exp(self.z_log_sigma_sq_pos)), eps)
+                )
 
-        self.x_reconstr_mean = self._generator_network(network_weights["weights_gener"], network_weights["biases_gener"])
+        self.x_reconstr_mean_pos = self._generator_network(
+                network_weights["weights_gener"], 
+                network_weights["biases_gener"],
+                self.z_pos)
+        
+        # graph for neg_example
+        self.z_mean_neg, self.z_log_sigma_sq_neg = self._recognition_network(
+                network_weights["weights_recog"], 
+                network_weights["biases_recog"], 
+                self.neg_example)
+
+        self.z_neg = tf.add(
+                self.z_mean_neg, 
+                tf.mul(tf.sqrt(tf.exp(self.z_log_sigma_sq_neg)), eps)
+                )
+
+        self.x_reconstr_mean_neg = self._generator_network(
+                network_weights["weights_gener"], 
+                network_weights["biases_gener"],
+                self.z_neg)
+
+    def _recognition_network(self, weights, biases, x):
+
+        layer_1 = self.transfer_fct(tf.add(
+            tf.matmul(x, weights['h1']), 
+            biases['b1'])) 
+        layer_2 = self.transfer_fct(tf.add(
+            tf.matmul(layer_1, weights['h2']), 
+            biases['b2'])) 
+        z_mean = tf.add(
+                tf.matmul(layer_2, weights['out_mean']), 
+                biases['out_mean'])
+        z_log_sigma_sq = tf.add(
+                tf.matmul(layer_2, weights['out_log_sigma']), 
+                biases['out_log_sigma'])
+        return (z_mean, z_log_sigma_sq)
+
+    def _generator_network(self, weights, biases, z):
+
+        layer_1 = self.transfer_fct(tf.add(
+            tf.matmul(z, weights['h1']), 
+            biases['b1'])) 
+        layer_2 = self.transfer_fct(tf.add(
+            tf.matmul(layer_1, weights['h2']), 
+            biases['b2'])) 
+        x_reconstr_mean = tf.nn.sigmoid(tf.add(
+            tf.matmul(layer_2, weights['out_mean']), 
+            biases['out_mean']))
+        return x_reconstr_mean
             
+    def _create_log_likelihood(self):
+        self.ll_pos = tf.reduce_sum(
+                self.pos_example * tf.log(1e-10 + self.x_reconstr_mean_pos) + (1-self.pos_example) * tf.log(1e-10 + 1 - self.x_reconstr_mean_pos),
+                1
+                )
+        self.ll_neg = tf.reduce_sum(
+                self.neg_example * tf.log(1e-10 + self.x_reconstr_mean_neg) + (1-self.neg_example) * tf.log(1e-10 + 1 - self.x_reconstr_mean_neg),
+                1
+                )
+      
+    def reconstruct(self, X):
+        samples = tf.select(
+                tf.greater(
+                    tf.random_uniform(tf.shape(self.pos_example)), 
+                    self.x_reconstr_mean_pos
+                    ),
+                tf.ones_like(self.pos_example),
+                tf.zeros_like(self.pos_example)
+                )
+        return self.sess.run(samples, {self.pos_example:X})
+    
     def _initialize_weights(self, n_hidden_recog_1, n_hidden_recog_2, 
                             n_hidden_gener_1,  n_hidden_gener_2, 
                             n_input, n_z):
         all_weights = dict()
         all_weights['weights_recog'] = {
-            'h1': tf.Variable(xavier_init(n_input, n_hidden_recog_1)),
-            'h2': tf.Variable(xavier_init(n_hidden_recog_1, n_hidden_recog_2)),
-            'out_mean': tf.Variable(xavier_init(n_hidden_recog_2, n_z)),
-            'out_log_sigma': tf.Variable(xavier_init(n_hidden_recog_2, n_z))}
+            'h1': tf.Variable(xavier_init(n_input, n_hidden_recog_1), trainable=self.trainable),
+            'h2': tf.Variable(xavier_init(n_hidden_recog_1, n_hidden_recog_2), trainable=self.trainable),
+            'out_mean': tf.Variable(xavier_init(n_hidden_recog_2, n_z), trainable=self.trainable),
+            'out_log_sigma': tf.Variable(xavier_init(n_hidden_recog_2, n_z), trainable=self.trainable)}
         all_weights['biases_recog'] = {
-            'b1': tf.Variable(tf.zeros([n_hidden_recog_1], dtype=tf.float32)),
-            'b2': tf.Variable(tf.zeros([n_hidden_recog_2], dtype=tf.float32)),
-            'out_mean': tf.Variable(tf.zeros([n_z], dtype=tf.float32)),
-            'out_log_sigma': tf.Variable(tf.zeros([n_z], dtype=tf.float32))}
+            'b1': tf.Variable(tf.zeros([n_hidden_recog_1], dtype=tf.float32), trainable=self.trainable),
+            'b2': tf.Variable(tf.zeros([n_hidden_recog_2], dtype=tf.float32), trainable=self.trainable),
+            'out_mean': tf.Variable(tf.zeros([n_z], dtype=tf.float32), trainable=self.trainable),
+            'out_log_sigma': tf.Variable(tf.zeros([n_z], dtype=tf.float32), trainable=self.trainable)}
         all_weights['weights_gener'] = {
-            'h1': tf.Variable(xavier_init(n_z, n_hidden_gener_1)),
-            'h2': tf.Variable(xavier_init(n_hidden_gener_1, n_hidden_gener_2)),
-            'out_mean': tf.Variable(xavier_init(n_hidden_gener_2, n_input)),
-            'out_log_sigma': tf.Variable(xavier_init(n_hidden_gener_2, n_input))}
+            'h1': tf.Variable(xavier_init(n_z, n_hidden_gener_1), trainable=self.trainable),
+            'h2': tf.Variable(xavier_init(n_hidden_gener_1, n_hidden_gener_2), trainable=self.trainable),
+            'out_mean': tf.Variable(xavier_init(n_hidden_gener_2, n_input), trainable=self.trainable),
+            }
         all_weights['biases_gener'] = {
-            'b1': tf.Variable(tf.zeros([n_hidden_gener_1], dtype=tf.float32)),
-            'b2': tf.Variable(tf.zeros([n_hidden_gener_2], dtype=tf.float32)),
-            'out_mean': tf.Variable(tf.zeros([n_input], dtype=tf.float32)),
-            'out_log_sigma': tf.Variable(tf.zeros([n_input], dtype=tf.float32))}
+            'b1': tf.Variable(tf.zeros([n_hidden_gener_1], dtype=tf.float32), trainable=self.trainable),
+            'b2': tf.Variable(tf.zeros([n_hidden_gener_2], dtype=tf.float32), trainable=self.trainable),
+            'out_mean': tf.Variable(tf.zeros([n_input], dtype=tf.float32), trainable=self.trainable),
+            }
         return all_weights
-            
-    def _recognition_network(self, weights, biases):
-
-        layer_1 = self.transfer_fct(tf.add(tf.matmul(self.x, weights['h1']), biases['b1'])) 
-        layer_2 = self.transfer_fct(tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])) 
-        z_mean = tf.add(tf.matmul(layer_2, weights['out_mean']), biases['out_mean'])
-        z_log_sigma_sq = tf.add(tf.matmul(layer_2, weights['out_log_sigma']), biases['out_log_sigma'])
-        return (z_mean, z_log_sigma_sq)
-
-    def _generator_network(self, weights, biases):
-
-        layer_1 = self.transfer_fct(tf.add(tf.matmul(self.z, weights['h1']), biases['b1'])) 
-        layer_2 = self.transfer_fct(tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])) 
-        x_reconstr_mean = tf.nn.sigmoid(tf.add(tf.matmul(layer_2, weights['out_mean']), biases['out_mean']))
-        return x_reconstr_mean
-            
-    def _create_loss_optimizer(self):
-        reconstr_term = tf.reduce_sum(
-                self.x * tf.log(1e-10 + self.x_reconstr_mean)+ (1-self.x) * tf.log(1e-10 + 1 - self.x_reconstr_mean),
-                1
-                )
-
-        prior_term = tf.reduce_sum(
-                0.5 * (1 + self.z_log_sigma_sq - tf.square(self.z_mean) - tf.exp(self.z_log_sigma_sq)),
-                1
-                )
-
-        self.lb = tf.reduce_mean(prior_term + reconstr_term)
-        self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(tf.neg(self.lb))
-        
-    def partial_fit(self, X):    
-
-        opt, lb = self.sess.run((self.optimizer, self.lb), 
-                                  feed_dict={self.x: X})
-        return lb
-    
-    def transform(self, X):
-
-        return self.sess.run(self.z_mean, feed_dict={self.x: X})
-    
-    def generate(self, z_mu=None):
-
-        if z_mu is None:
-            z_mu = np.random.normal(size=[self.batch_size, self.network_architecture["n_z"]])
-
-        return self.sess.run(self.x_reconstr_mean, 
-                             feed_dict={self.z: z_mu})
-    
-    def reconstruct(self, X):
-
-        return self.sess.run(self.x_reconstr_mean, 
-                             feed_dict={self.x: X})
+ 
